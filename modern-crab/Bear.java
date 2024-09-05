cffd26491c50575d5f7b528fe0e69fb369946553
James Floyd <jamesaydenfloyd2@gmail.com>
2024-09-05 16:38:02 -0400

    lab 2

---
+++ modern-crab/Bear.java
@@ -0,0 +1,26 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Bear extends Actor
+{
+
+    /**
+     * Act - do whatever the Bear to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);
+        if (Greenfoot.isKeyDown("a")) {
+            turn(-3);
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            turn(3);
+        }
+        setLocation(3, 7);
+    }
+}
